4bba910cf5ebe51ee9593869dd7ff13230b40972
joara-thrlaw <arjoe207@gmail.com>
2026-03-17 10:38:51 +0900

    feat: 로그인 페이지 이메일 기억하기 기능 추가

--- src/app/(auth)/login/_libs/login.schema.ts
+++ src/app/(auth)/login/_libs/login.schema.ts
@@ -6,6 +6,7 @@ export const signInSchema = z.object({
     .nonempty("이메일을 입력해주세요.")
     .email("잘못된 이메일 형식입니다."),
   password: z.string().min(1, "비밀번호를 입력해 주세요"),
+  rememberEmail: z.boolean().default(false),
 });
 
 export type signInValues = z.infer<typeof signInSchema>;
--- src/app/(auth)/login/_libs/useLogin.ts
+++ src/app/(auth)/login/_libs/useLogin.ts
@@ -1,10 +1,13 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { signInSchema, signInValues } from "./login.schema";
+import { useEffect } from "react";
 
 export function useLogin() {
   const {
     register,
+    control,
+    setValue,
     handleSubmit,
     formState: { errors, isValid, touchedFields },
   } = useForm({
@@ -18,10 +21,21 @@ export function useLogin() {
 
   const onSubmit = (data: signInValues) => {
     console.log(data);
+    if (data.rememberEmail) localStorage.setItem("rememberedEmail", data.email);
+    else localStorage.removeItem("rememberedEmail");
   };
+
+  useEffect(() => {
+    const savedEmail = localStorage.getItem("rememberedEmail");
+    if (savedEmail) {
+      setValue("email", savedEmail);
+      setValue("rememberEmail", true);
+    }
+  }, [setValue]);
 
   return {
     register,
+    control,
     errors,
     isValid,
     touchedFields,
