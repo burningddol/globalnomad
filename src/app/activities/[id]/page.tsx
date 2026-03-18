ae0a4117c11ab408612aba65848acbba6b8e147a
junhub98 <helpwoodjs@icloud.com>
2026-03-18 15:18:50 +0900

    feat: KAKAOMAP sdk

--- src/app/activities/[id]/page.tsx
+++ src/app/activities/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { ActivityHeader } from "./_components/ActivityHeader";
 import { BannerImages } from "./_components/BannerImages";
 import { Description } from "./_components/Description";
+import KakaoMap from "./_components/KakaoMap";
 import type { Activity, Reviews } from "@/commons/consts/activity";
 
 export default function ActivityDetailPage() {
@@ -19,8 +20,10 @@ export default function ActivityDetailPage() {
         <div className="h-200 flex items-center hidden xl:block">캘린더란</div>
       </div>
 
-      <div className="xl:col-start-1 flex justify-center self-start">
+      <div className="xl:col-start-1 flex flex-col self-start">
         <Description content={MOCK_ACTIVITY.description} />
+
+        <KakaoMap address={MOCK_ACTIVITY.address} title={MOCK_ACTIVITY.title} />
       </div>
     </div>
   );
